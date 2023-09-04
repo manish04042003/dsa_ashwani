c898cb47258de6d083e4949cc48ac2bc95dc098b
manish04042003 <manishkumar817835@gmail.com>
2023-09-04 20:06:52 +0530

    Initial commit

---
+++ src/apnacollegePractice/demicalToBinary.java
@@ -0,0 +1,16 @@
+package apnacollegePractice ;
+
+public  class demicalToBinary {
+
+    public static void main(String[] args) {
+        int n = 8;
+        
+        while(n>0){
+
+
+            n=n/2;
+        }
+    }
+}
+
+
