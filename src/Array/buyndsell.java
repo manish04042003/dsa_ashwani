7ef8a37e369294dc3c52cfc2aa3d40fa750958cd
Manish kumar <96832428+manish04042003@users.noreply.github.com>
2023-11-06 10:32:11 +0530

    buy and sell stoke

---
+++ src/Array/buyndsell.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class buyndsell {
+    public static void main(String[] args) {
+        int[] arr = {7,1,5,3,6};
+        int max = Integer.MIN_VALUE ;
+
+        for (int i =0; i < arr.length-1;i++){
+            for (int j =i+1; j< arr.length; j++){
+                if (arr[j]-arr[i]>max){
+                    max=arr[j]-arr[i];
+                }
+            }
+        }
+
+        System.out.println(max);
+    }
+}
