6a4aacdf2365f18ce483404b5df6819e0c806cc1
Manish kumar <96832428+manish04042003@users.noreply.github.com>
2023-11-08 03:13:12 +0530

    Tower of honoi

---
+++ src/Recursion/TowerOfHonoi.java
@@ -0,0 +1,29 @@
+package Recursion;
+
+public class TowerOfHonoi {
+    public static void main(String[] args) {
+        int n = 3 ;
+        int a = 10;
+        int b = 11;
+        int c = 12;
+
+        System.out.println(tower(n,a,b,c));
+    }
+
+    private static int tower(int n, int a, int b, int c) {
+        if (n==0){
+            return 0;
+        }
+        int s1 = tower(n-1,a,c,b);
+//       System.out.println(""+ n + "["+ a+"->"+b+"]");
+        System.out.println("move disk "+ n + " from rod "+ a+" to "+b);
+       int s2 = tower(n-1,c,b,a);
+
+        if (n==1){
+            return 1;
+        }
+
+
+        return s1 + s2 + 1  ;
+    }
+}
