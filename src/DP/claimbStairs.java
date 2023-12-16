78291cb531199dc607f255e550358fe04d88e09b
Manish kumar <96832428+manish04042003@users.noreply.github.com>
2023-12-16 16:36:20 +0530

    claimbStairs

--- src/DP/claimbStairs.java
+++ src/DP/claimbStairs.java
@@ -3,8 +3,24 @@ package DP;
 public class claimbStairs {
     public static void main(String[] args) {
 
-        int ans = countClaimbStairs(4,0);
-        System.out.println(ans);
+//        int ans = countClaimbStairs(4,0);
+//        int ans = countClaimbStairswithjumps(10,new int[]{3,3,0,2,1,2,4,2,0,0});
+
+//        System.out.println(ans);
+    }
+    public static  int countClaimbStairswithjumps(int n, int[] arr){
+        int[] table = new int[n +1];
+        table[n] = 1;
+        for (int i =n-1 ; i>=0;i--){
+            int count = 0;
+            for(int j =1 ; j<=arr[i];j++){
+                if(i+j<table.length){
+                    count = count+ table[i+j];
+                }
+            }
+            table[i]=count;
+        }
+        return table[0];
     }
     public  static int countClaimbStairs(int n,int s){
 
@@ -20,4 +36,5 @@ public class claimbStairs {
 
         return one + two + three;
     }
+
 }
